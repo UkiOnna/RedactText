aa3bf03c80af4f64969f8ed4a41806592e989aca
UkiOnna <sikirovtemirlan@mail.ru>
2018-01-16 15:05:21 +0600

    dfgd

---
+++ TextRedactor/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextRedactor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string paste;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CutClick(object sender, RoutedEventArgs e)
+        {
+            if (Text.SelectedText == string.Empty)
+            {
+
+            }
+            else
+            {
+                paste = Text.SelectedText;
+                Text.SelectedText = "";
+            }
+        }
+
+        private void PasteClick(object sender, RoutedEventArgs e)
+        {
+            Text.SelectedText = Text.SelectedText+paste;
+        }
+
+        private void CopyClick(object sender, RoutedEventArgs e)
+        {
+            if (Text.SelectedText == string.Empty)
+            {
+
+            }
+            else
+            {
+                paste = Text.SelectedText;
+            }
+        }
+    }
+}
